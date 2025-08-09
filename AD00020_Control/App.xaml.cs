640b1d74ed4d3b2e54d3b2beb3862fafe3f40ade
sashi <satoshi3104.nishi@gmail.com>
2025-08-09 17:15:22 +0900

    Mutex

--- AD00020_Control/App.xaml.cs
+++ AD00020_Control/App.xaml.cs
@@ -9,6 +9,24 @@ namespace AD00020_Control
     /// </summary>
     public partial class App : Application
     {
-    }
+        private static Mutex _mutex;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _mutex = new Mutex(true, "AD00020_Control", out var createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show(
+                    "AD00020_Control is already running.",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+    }
 }
